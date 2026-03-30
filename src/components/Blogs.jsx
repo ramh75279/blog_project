27aeb796c14271c2dd5a3c3b71565b8e316d3c40
ramh75279 <ramh75379@gmail.com>
2026-03-30 08:08:57 +0530

    Update Blogs.jsx

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -42,10 +42,10 @@ function Blogs() {
 
     const handleLike = async (blog_id) => {
         try {
-            const response = await axios.patch(`http://localhost:5000/api/blogs/like/${blog_id}`);
+            const response = await axios.patch(`https://blog-project-backend-3-mnlx.onrender.com/api/blogs/like/${blog_id}`);
             // After successfully updating the likes count in the backend, fetch the updated list of blogs
             if (response.status === 200) {
-                axios.get("http://localhost:5000/api/blogs").then((res) => {
+                axios.get("https://blog-project-backend-3-mnlx.onrender.com/api/blogs").then((res) => {
                     console.log(res.data)
                     setBlogs(res.data)
                 }).catch(() => {
@@ -64,7 +64,7 @@ function Blogs() {
 
 
         const likes = 0
-        axios.post("http://localhost:5000/api/blogs", { newTitle, date, newContent, likes }).then((res) => {
+        axios.post("https://blog-project-backend-3-mnlx.onrender.com/api/blogs", { newTitle, date, newContent, likes }).then((res) => {
             console.log(res.data)
 
             axios.get("http://localhost:5000/api/blogs").then((res) => {
